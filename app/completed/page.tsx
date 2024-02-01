d276a815fcbc340ff433e42b5783f932b9d61b55
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-02-01 21:26:37 +0200

    Added sidebar, added 3 templates for pages: important, incomplete, complted

---
+++ app/completed/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import React from 'react'
+import { useGlobalState } from '../context/globalProvider'
+
+function page() {
+	return <div>important</div>
+}
+
+export default page
---
+++ app/incomplete/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import React from 'react'
+import { useGlobalState } from '../context/globalProvider'
+
+function page() {
+	return <div>incomplete</div>
+}
+
+export default page

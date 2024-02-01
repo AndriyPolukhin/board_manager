8129ee01a4dd8ee1dbe223d236c341816db0d40a
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-02-01 23:59:54 +0200

    Added basic Tasks component

---
+++ app/components/Tasks/Tasks.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { useGlobalState } from '@/app/context/globalProvider'
+import React from 'react'
+import styled from 'styled-components'
+
+const Tasks = () => {
+	const { themes } = useGlobalState()
+	return <TaskStyled theme={themes}>Tasks</TaskStyled>
+}
+
+const TaskStyled = styled.main`
+	position: relative;
+	height: 100%;
+	width: 100%;
+	padding: 2rem;
+	background-color: ${(props) => props.theme.colorBg2};
+	border: 2px solid ${(props) => props.theme.borderColor2};
+	border-radius: 1rem;
+	overflow-y: auto;
+
+	&::-webkit-scrollbar {
+		width: 0.5rem;
+	}
+`
+
+export default Tasks

3cb33b1c50b6062c21b4cd8dd729ce4eadb2adfd
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-02-04 21:11:41 +0200

    Updated Sidebar component

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -1,23 +1,39 @@
 'use client'
 import React from 'react'
-import { useGlobalState } from '@/app/context/globalProvider'
-import { usePathname, useRouter } from 'next/navigation'
-import Image from 'next/image'
 import styled from 'styled-components'
+import { useGlobalState } from '@/app/context/globalProvider'
+import Image from 'next/image'
+
 import menu from '../../utils/menu'
 import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
+import Button from '../Button/Button'
+import { arrowLeft, bars, logout } from '@/app/utils/Icons'
+import { UserButton, useClerk, useUser } from '@clerk/nextjs'
 
 function Sidebar() {
-	const { theme } = useGlobalState()
+	const { theme, collapsed, collapseMenu } = useGlobalState()
+	const { signOut } = useClerk()
+	const { user } = useUser()
+
+	const { firstName, lastName, imageUrl } = user || {
+		firstName: '',
+		lastName: '',
+		imageUrl: '',
+	}
 
 	const router = useRouter()
 	const pathname = usePathname()
+
 	const handleClick = (link: string) => {
 		router.push(link)
 	}
 
 	return (
-		<SidebarStyled theme={theme}>
+		<SidebarStyled theme={theme} collapsed={collapsed}>
+			<button className='toggle-nav' onClick={collapseMenu}>
+				{collapsed ? bars : arrowLeft}
+			</button>
 			<div className='profile'>
 				<div className='profile-overflay'></div>
 				<div className='image'>
@@ -29,9 +45,11 @@ function Sidebar() {
 						priority
 					/>
 				</div>
-				<h1>
-					<span>Andriy</span>
-					<span>Polukhin</span>
+				<div className='user-btn absolute z-20 top-0 w-full h-full'>
+					<UserButton />
+				</div>
+				<h1 className='capitalize'>
+					{firstName} {lastName}
 				</h1>
 			</div>
 			<ul className='nav-items'>
@@ -46,23 +64,85 @@ function Sidebar() {
 					</li>
 				))}
 			</ul>
+			<div className='sign-out relative m-6'>
+				<Button
+					name={'Sign Out'}
+					type={'submit'}
+					padding={'0.4rem 0.8rem'}
+					borderRad={'0.8rem'}
+					fw={'500'}
+					fs={'1.2rem'}
+					icon={logout}
+					click={() => {
+						signOut(() => router.push('/signin'))
+					}}
+				/>
+			</div>
 			<button></button>
 		</SidebarStyled>
 	)
 }
 
-const SidebarStyled = styled.nav`
+const SidebarStyled = styled.nav<{ collapsed: boolean }>`
 	position: relative;
 	width: ${(props) => props.theme.sidebarWidth};
 	background-color: ${(props) => props.theme.colorBg2};
-	border: 2px solid ${(props) => props.theme.borderColor};
+	border: 2px solid ${(props) => props.theme.borderColor2};
 	border-radius: 1rem;
 
 	display: flex;
 	flex-direction: column;
 	justify-content: space-between;
 
-	color: ${(props) => props.theme.colorGray3};
+	color: ${(props) => props.theme.colorGrey3};
+
+	@media screen and (max-width: 768px) {
+		position: fixed;
+		height: calc(100vh - 2rem);
+		z-index: 100;
+
+		transition: all 0.3s cubic-bezier(0.53, 0.21, 0, 1);
+		transform: ${(props) =>
+			props.collapsed ? 'translateX(-107%)' : 'translateX(0)'};
+
+		.toggle-nav {
+			display: block !important;
+		}
+	}
+
+	.toggle-nav {
+		display: none;
+		padding: 0.8rem 0.9rem;
+		position: absolute;
+		right: -69px;
+		top: 1.8rem;
+
+		border-top-right-radius: 1rem;
+		border-bottom-right-radius: 1rem;
+
+		background-color: ${(props) => props.theme.colorBg2};
+		border-right: 2px solid ${(props) => props.theme.borderColor2};
+		border-top: 2px solid ${(props) => props.theme.borderColor2};
+		border-bottom: 2px solid ${(props) => props.theme.borderColor2};
+	}
+
+	.user-btn {
+		.cl-rootBox {
+			width: 100%;
+			height: 100%;
+
+			.cl-userButtonBox {
+				width: 100%;
+				height: 100%;
+
+				.cl-userButtonTrigger {
+					width: 100%;
+					height: 100%;
+					opacity: 0;
+				}
+			}
+		}
+	}
 
 	.profile {
 		margin: 1.5rem;
